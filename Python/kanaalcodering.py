143e51a151ce132072ef75bf448a86a6307f24a3
yanis.vaneeckhout <yanis.vaneeckhout@vtk.ugent.be>
2021-12-07 00:22:09 +0100

    bug-fixes

--- Python/kanaalcodering.py
+++ Python/kanaalcodering.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.ma.core import bitwise_xor
 import numpy.matlib
 import math
 from scipy import signal
@@ -34,34 +35,74 @@ class Kanaalcodering():
     
     # functie die de decoder van de uitwendige code implementeert
     def decodeer_uitwendig(bitstring):
-        # bitstring : vector met gecodeerde bits
-        bitstring_vec = np.array(bitstring)
-        bool_fout = 0
-        bitdec = np.zeros((1,10))
+        # # bitstring : vector met gecodeerde bits
+        # bitstring_vec = np.array(bitstring)
+        # bool_fout = 0
+        # bitdec = np.zeros((1,10))
 
         H = np.array([[1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],[0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0 ],[0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1],[1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0]])
         
-        # Implementeer vanaf hier
-        s = np.matmul(bitstring_vec, np.transpose(H))
-
-        if (s == np.zeros((1, 4))):
-            bool_fout = 0
-        else: bool_fout = 1
+        # # Implementeer vanaf hier
         
-        #syndroom tabel
+        bitstring_vec = np.array(bitstring)
+        s = np.fmod(np.matmul(bitstring_vec,np.transpose(H)),2)
+       
+        bool_fout = False if (np.all(s == 0)) else True
+        
 
-        syn = np.array([[0,0,0,0],[1,0,0,1],[1,0,0,0],[0,1,1,0],[1,1,1,0],[1,1,0,0],[0,1,0,0],[0,0,1,0],[1,1,1,1],[0,0,0,1],[0,0,1,1],[1,1,0,1],[0,1,1,1],[0,1,0,1],[1,0,1,0],[1,0,1,1]])
-        err = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,1,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,1,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,1,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,1,0,0,0,0,0,0,0]])
+        syn = np.array([
+            [0,0,0,0],
+            [1,0,0,1],
+            [1,0,0,0],
+            [0,1,1,0],
+            [1,1,1,0],
+            [1,1,0,0],
+            [0,1,0,0],
+            [0,0,1,0],
+            [1,1,1,1],
+            [0,0,0,1],
+            [0,0,1,1],
+            [1,1,0,1],
+            [0,1,1,1],
+            [0,1,0,1],
+            [1,0,1,0],
+            [1,0,1,1]
+        ])
 
-        if (s == syn[0]):
-            bool_fout = 0
-            bitdec = bitstring_vec
+
+        err = np.array([
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+            [1,0,0,0,0,0,1,0,0,0,0,0,0,0]
+        ])
+
+        e = np.zeros(14)
+        r_c = np.zeros(14)
+        if (bool_fout):
+            index = 0
+            for i in range(16):
+                if np.all(s == syn[i]):
+                    index = i
+            
+            e = err[index]
+            r_c = np.bitwise_xor(bitstring_vec, e)
         else:
-            bool_fout = 1
-            e = err[np.where(s = syn)]
-            bitdec = np.mdo((bitstring_vec + e))
-                
-
+            r_c = bitstring_vec
+        idx = [0,10,2,3,4,11,12,7,13,9]
+        bitdec = r_c[idx]
         # bitdec : vector met gedecodeerde bits bij volledige foutcorrectie
         # bool_fout : 1 als een fout gedetecteerd is bij zuivere foutdetectie, 0 anders        
         return(bitdec,bool_fout)
@@ -105,6 +146,8 @@ print(a)
 print(Kanaalcodering.decodeer_inwendig(a, [1,1,0,1,0,1]))
 
 print("---Test 3: Blokcode ---")
-a = Kanaalcodering.encodeer_uitwendig([1,0,1,0,1,1,0,1,1,0])
+a = Kanaalcodering.encodeer_uitwendig([1,0,0,0,1,1,0,1,1,0])
 print(a)
-print(Kanaalcodering.decodeer_uitwendig(a))
+b = Kanaalcodering.decodeer_uitwendig(a)
+print(b)
+    
