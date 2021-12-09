0a56a6f2b56d1d4211ab3dc52718f0fe1a7d1c6f
dagod <dario.god@outlook.be>
2021-12-09 18:06:23 +0100

    moddet in config 1

--- Python/run_configuratie_1.py
+++ Python/run_configuratie_1.py
@@ -44,15 +44,31 @@ def run_broncodering():
 
     data_encoded_lijst = []
     for bit in data_encoded_str:
-        data_encoded_lijst.append(bit)
+        data_encoded_lijst.append(int(bit))
 
     return data_encoded_lijst
 
-def run_kanaalcodering():
-    return 1
 
 def run_moddet():
-    return 1
+    obj = ModDet()
+
+    #variables:
+    constellatie = 'BPSK'
+    T = 10**(-6)
+    Ns = 6
+    f0 = 2*10**6
+    alpha = 0.5
+    Lf = 10
+    N0 = 0.7
+    sigma = math.sqrt(N0*Ns/2/T)
+    hch = 1
+    theta = math.pi / 16
+
+    bitlist = run_broncodering()
+
+    bitarray_out = obj.modulation_detection(bitlist, constellatie, T, Ns, f0, alpha, Lf, N0, hch, theta)
+
+    return bitarray_out
 
 warnings.simplefilter('ignore') # ignore warnings of integral
 
